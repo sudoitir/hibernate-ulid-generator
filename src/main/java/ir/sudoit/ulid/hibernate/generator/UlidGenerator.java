fce73912a2b91b876a97db36114b988b59845dc9
Amirabdollahi <sudoit.ir@gmail.com>
2024-05-27 09:55:39 +0330

    refactor: Add support of id field in super classes

--- src/main/java/ir/sudoit/ulid/hibernate/generator/UlidGenerator.java
+++ src/main/java/ir/sudoit/ulid/hibernate/generator/UlidGenerator.java
@@ -7,27 +7,59 @@ import org.hibernate.engine.spi.SharedSessionContractImplementor;
 import org.hibernate.id.IdentifierGenerator;
 
 import java.lang.reflect.Field;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
 
 public class UlidGenerator implements IdentifierGenerator {
+
+    private final ConcurrentMap<Class<?>, Field> idFieldCache = new ConcurrentHashMap<>();
 
     @Override
     public Object generate(SharedSessionContractImplementor session, Object object) {
         Class<?> entityClass = object.getClass();
-
-        Field idField = null;
-        for (Field field : entityClass.getDeclaredFields()) {
-            if (field.isAnnotationPresent(Id.class)) {
-                idField = field;
-                break;
-            }
-        }
+        Field idField = getIdField(entityClass);
 
         if (idField != null) {
-            Class<?> idType = idField.getType();
-            return generateValue(idType);
+            return generateValue(idField.getType());
         } else {
-            throw new HibernateException("No ID field found in the entity class");
+            throw new HibernateException("No ID field found in the entity class " + entityClass.getName());
         }
+    }
+
+    private Field getIdField(Class<?> entityClass) {
+        return idFieldCache.computeIfAbsent(entityClass, this::findIdField);
+    }
+
+    private Field findIdField(Class<?> entityClass) {
+        // Check superclasses first
+        Field idField = findIdFieldInClassHierarchy(entityClass.getSuperclass());
+
+        // Check current class if not found in superclasses
+        if (idField == null) {
+            idField = findIdFieldInClass(entityClass);
+        }
+
+        return idField;
+    }
+
+    private Field findIdFieldInClassHierarchy(Class<?> cls) {
+        while (cls != null) {
+            Field idField = findIdFieldInClass(cls);
+            if (idField != null) {
+                return idField;
+            }
+            cls = cls.getSuperclass();
+        }
+        return null;
+    }
+
+    private Field findIdFieldInClass(Class<?> cls) {
+        for (Field field : cls.getDeclaredFields()) {
+            if (field.isAnnotationPresent(Id.class)) {
+                return field;
+            }
+        }
+        return null;
     }
 
     public Object generateValue(Class<?> type) {
